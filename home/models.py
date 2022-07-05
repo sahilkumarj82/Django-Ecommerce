8a407e9750ab6f726b39f83711824c5e68fdc374
sahilkumarj82 <sahilkumarj82@gmail.com>
2022-07-05 04:40:49 -0700

    Django-Ecommerce

---
+++ home/models.py
@@ -0,0 +1,86 @@
+from distutils.command.upload import upload
+from pydoc import describe
+from pyexpat import model
+from statistics import mode
+from telnetlib import STATUS
+from unicodedata import category
+from django.db import models
+
+# Create your models here.
+# Model for Category
+class Category(models.Model):
+    name = models.CharField(max_length=100)
+    icon = models.CharField(max_length=200)
+    slug = models.TextField(unique = True)
+
+    def __str__(self):
+        return self.name
+
+#Model for Subcategory of Category
+class SubCategory(models.Model):
+    name = models.CharField(max_length=100)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    icon = models.CharField(max_length=200,blank=True)
+    slug = models.TextField(unique = True)
+
+    def __str__(self):
+        return self.name
+#Model for Slider
+STATUS = (('active','Active'),('','Default'))
+class Slider(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to = 'media')#upload Image code
+    text = models.TextField(blank = True)
+    rank = models.IntegerField()
+    status = models.CharField(choices= STATUS,blank=True,max_length=100)
+
+    def __str__ (self):
+        return self.name
+
+#Model for Ads
+class Ad(models.Model):
+    name = models.CharField(max_length=200)
+    image = models.ImageField(upload_to = 'media')
+    text = models.TextField(blank=True)
+    rank = models.IntegerField()
+    def __str__(self):
+        return self.name
+
+#models for brand
+class Brand(models.Model):
+    name = models.CharField(max_length=400)
+    image = models.ImageField(upload_to = 'media')
+    rank = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+
+#for product
+LABELS = (('new','New'),('hot','Hot'),('sale','Sale'),('','default'))
+STOCK = (('In Stock','In Stock'),('Out Stock','Out Stock'))
+class Product(models.Model):
+    name = models.CharField(max_length=400)
+    price = models.IntegerField()
+    discount_price = models.IntegerField(default=0)
+    category = models.ForeignKey(Category,on_delete=models.CASCADE)
+    sub_category = models.ForeignKey(SubCategory,on_delete=models.CASCADE)
+    image = models.ImageField(upload_to = 'media')#upload Image code
+    description = models.TextField(blank = True)
+    specification = models.TextField(blank=True)
+    slug = models.TextField(unique = True)
+    labels = models.CharField(choices= LABELS,max_length=100)
+    stock = models.CharField(choices= STOCK,max_length=100)
+
+    def __str__(self):
+        return self.name
+
+
+class Review(models.Model):
+    name = models.CharField(max_length=400)
+    email = models.EmailField(max_length=400)
+    Review = models.TextField(blank=True)
+    slug = models.TextField()
+    date = models.DateField()
+
+    def __str__(self):
+        return self.name
---
+++ home/views.py
@@ -0,0 +1,19 @@
+import imp
+from django.shortcuts import render
+from .models import *
+from django.views.generic import View
+# Create your views here.
+
+class Base(View): # --> Base for common page and View --> 
+    views = {} # 
+class HomeView(Base):
+    def get(self,request):
+        self.views['categories']  = Category.objects.all() # show all categories
+        self.views['sliders'] = Slider.objects.all() # get all slider and show
+        self.views['ads'] = Ad.objects.all() # get ads and show
+        self.views['brands'] = Brand.objects.all() # get all slider and show
+        self.views['hots'] = Product.objects.filter(labels = 'hot') # filtering products from labels from models
+        self.views['sales'] = Product.objects.filter(labels = 'sale') # filtering products from labels from models
+        self.views['news'] = Product.objects.filter(labels = 'new') # filtering products from labels from models
+        
+        return render(request,'index.html') # --> for render
